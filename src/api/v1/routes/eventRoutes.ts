7b8b491f3cedd43ea04a37921011528875bfa7ad
karanvir singh <ksingh34@academic.rrc.ca>
2026-02-19 17:15:53 -0600

    feat: add PUT endpoint.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { createEvent } from "../controllers/eventController";
+
 import { validate } from "../middleware/validate";
 import { createEventSchema } from "../validation/eventvalidation";
+import { createEvent, getAllEvents, getEventById, updateEvent } from "../controllers/eventController";
 
 const router = Router();
 
 router.post("/", validate(createEventSchema), createEvent);
-
+router.get("/", getAllEvents);
+router.get("/:id", getEventById);
+router.put("/:id", updateEvent);
 export default router;
