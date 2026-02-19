46fb8e557929f6087ee109ce2fd762d291acfb13
karanvir singh <ksingh34@academic.rrc.ca>
2026-02-19 17:38:59 -0600

    feat: Add DELETE endpoint.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { validate } from "../middleware/validate";
 import { createEventSchema } from "../validation/eventvalidation";
-import { createEvent, getAllEvents, getEventById, updateEvent } from "../controllers/eventController";
+import { createEvent, getAllEvents, getEventById, updateEvent, deleteEvent } from "../controllers/eventController";
 
 const router = Router();
 
@@ -10,4 +10,5 @@ router.post("/", validate(createEventSchema), createEvent);
 router.get("/", getAllEvents);
 router.get("/:id", getEventById);
 router.put("/:id", updateEvent);
+router.delete("/:id", deleteEvent);
 export default router;
