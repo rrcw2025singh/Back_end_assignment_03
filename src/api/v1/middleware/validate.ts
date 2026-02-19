29aaa810ddd4fd8bec0d8b5173e3a6a21b427e9b
karanvir singh <ksingh34@academic.rrc.ca>
2026-02-18 21:35:58 -0600

    feat: implement reusuable joi validation middleware.

---
+++ src/api/v1/middleware/validate.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { ObjectSchema } from "joi";
+
+export const validate = (schema: ObjectSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({
+        message: `Validation error: ${error.details[0].message}`
+      });
+    }
+
+    next();
+  };
+};
